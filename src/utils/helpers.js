6fadfa262662db49f0c2fb9d219fd5fdcfc825d2
Joseph Flatley <josephflatley@Josephs-MacBook-Pro.local>
2023-07-05 21:55:58 -0500

    Updates

--- src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-export function validateEmail(email) {
-    const regex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
-    return regex.test(String(email).toLowerCase());
-}
