016035361e993afcb4713ec5248eb18b7431a754
Joseph Flatley <josephflatley@Josephs-MacBook-Pro.local>
2023-07-05 20:46:55 -0500

    resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,10 @@
-import React from 'react';  
+/*import React from 'react';  
 
 function Resume() {
     return (
         <section>
             <h1>Resume</h1>
+           
             <p>Download my <a href="./assets/Resume.pdf" download>resume</a></p>
             <h2>Front-end Proficiencies</h2>
             <ul>
@@ -29,4 +30,45 @@ function Resume() {
     );
 }
 
-export default Resume;
+export default Resume; */
+
+function Resume() {
+    return (
+      <div className="columns">
+        <div className="column">
+          <p className="content is-medium">Résumé</p>
+          <hr />
+  
+          <a
+            className="button is-primary"
+            href={process.env.PUBLIC_URL + "./assets/Resume.pdf"}
+            target="_blank" rel="noreferrer"
+          >
+            <span className="icon">
+              <i className="fas fa-download"></i>
+            </span>
+            <span>Download My Resume</span>
+          </a>
+        </div>
+        <div className="column">
+          <p className="content is-medium">Skills</p>
+          <hr />
+          <ul>
+                <li style={{ textDecoration: 'underline', fontWeight: 'bold' }}>Front-end:</li>
+                <li>HTML, CSS, Javascript</li>
+                <li>JQuery, React</li>
+                <li>Responsive Design,</li>
+                <li>Bootstrap</li>
+                <hr/>
+                <li style={{ textDecoration: 'underline', fontWeight: 'bold' }}>Back-end:</li>
+                <li>APIs, Node, Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST, GraphQL</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Resume;
